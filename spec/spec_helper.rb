bd4d7b93c00e62f4a1ed0cb95798b5fac4fafb8b
gonzaloaparicio <gha.apa@gmail.com>
2012-11-14 12:02:08 -0200

    Update spec/spec_helper.rb
    Fixing error with JSON targets

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,4 @@ Ejemplo JSON:
 #{File.read("#{ENV['OMNITURE_TEST_PATH']}/targets.json.sample")}
 " if !File.exists?('targets.yml') && !File.exists?('targets.json')
 
-$targets = File.exists?('targets.yml') ? YAML.load_file('targets.yml')['targets'] : JSON.parse(File.read('targets.json'))
+$targets = File.exists?('targets.yml') ? YAML.load_file('targets.yml')['targets'] : JSON.parse(File.read('targets.json'))['targets']
